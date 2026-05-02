47f37237ea7c0cf3e19e137c4cc7600609a59bec
Akoh-0909 <ara.c.koh@gmail.com>
2026-05-03 01:45:36 +0900

    feat(mypage): 마이페이지 URL 라우팅 추가

--- config/urls.py
+++ config/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
   path("password-reset/", TemplateView.as_view(template_name="accounts/password_reset.html"), name="page-password-reset"),
   path("", TemplateView.as_view(template_name="chat/thread_list.html"), name="page-thread-list"),
   path("threads/<int:pk>/", TemplateView.as_view(template_name="chat/thread_detail.html"), name="page-thread-detail"),
+  path("mypage/", TemplateView.as_view(template_name="accounts/mypage.html"), name="page-mypage"),
 ]
